8ec59f9d9cb8c5ab4b563ecf61fc64fe7cad41b7
GabrielTefteller <gabriel287@revature.net>
2022-12-29 18:18:50 -0600

    Initial Commit

---
+++ src/main/java/app/HiScoreApp.java
@@ -0,0 +1,117 @@
+package app;
+
+import models.Score;
+import io.javalin.Javalin;
+import io.javalin.http.HttpStatus;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HiScoreApp
+{
+    public static int lastId = 0;
+
+    public static void main(String[] args)
+    {
+        Javalin app = Javalin.create();
+
+        Map<Integer, Score> scoreMap = new HashMap<>();
+
+        // create a new score
+        app.post("/scores", ctx ->
+        {
+            Score newScore = ctx.bodyAsClass(Score.class); // Unmarshalling
+            newScore.setId(lastId++);
+
+            scoreMap.put(newScore.getId(), newScore);
+
+            ctx.status(HttpStatus.CREATED);
+
+            ctx.json(newScore);
+        });
+
+        // return all scores
+        app.get("/scores", ctx ->
+        {
+            List<Score> scores = new ArrayList<>();
+            for (Score score : scoreMap.values()) {
+                scores.add(score);
+            }
+            ctx.json(scores);
+        });
+
+        // returns score by specified initials; not yet working
+        app.get("/scores?initials={AAA}", ctx ->
+        {
+           String searchInitials = ctx.queryParam("initials");
+           String initials;
+
+            for (int id = 0; id < scoreMap.size(); ++id)
+            {
+                initials = scoreMap.get(id).getInitials();
+                if (initials.equals(searchInitials))
+                {
+                    ctx.json(scoreMap.get(id));
+                }
+            }
+        });
+
+        // returns score by specified id; Returns 404 if score with ID not found
+        app.get("/scores/{id}", ctx ->
+        {
+            int scoreId = Integer.parseInt(ctx.pathParam("id"));
+            Score score = scoreMap.get(scoreId);
+
+            if (score == null)
+            {
+                ctx.status(HttpStatus.NOT_FOUND);
+                ctx.result("No score with ID " + scoreId + " found.");
+            }
+            else
+            {
+                ctx.json(score);
+            }
+        });
+
+        // replaces score with specified id; Returns 404 if score with ID not found
+        app.put("/scores/{id}", ctx ->
+        {
+            int scoreId = Integer.parseInt(ctx.pathParam("id"));
+            Score score = scoreMap.get(scoreId);
+
+            if (score == null)
+            {
+                ctx.status(HttpStatus.NOT_FOUND);
+                ctx.result("No score with ID " + scoreId + " found.");
+            }
+            else
+            {
+                Score newScore = ctx.bodyAsClass(Score.class); // Unmarshalling
+                newScore.setId(score.getId());
+                scoreMap.replace(scoreId, newScore);
+                ctx.json(newScore);
+            }
+        });
+
+        // deletes score with specified id; Returns 404 if score with ID not found
+        app.delete("/scores/{id}", ctx ->
+        {
+            int scoreId = Integer.parseInt(ctx.pathParam("id"));
+            Score score = scoreMap.get(scoreId);
+
+            if (score == null)
+            {
+                ctx.status(HttpStatus.NOT_FOUND);
+                ctx.result("No score with ID " + scoreId + " found.");
+            }
+            else
+            {
+                scoreMap.remove(scoreId);
+                ctx.result("Score with ID " + scoreId + " successfully deleted.");
+            }
+        });
+
+        app.start(8080);
+    }
+}
---
+++ src/main/java/models/Score.java
@@ -0,0 +1,50 @@
+package models;
+
+public class Score
+{
+    private int id;
+    private String initials;
+    private int points;
+
+    public Score() {}
+
+    public Score(int id, String initials, int points)
+    {
+        this.id = id;
+        this.initials = initials;
+        this.points = points;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getInitials() {
+        return initials;
+    }
+
+    public void setInitials(String initials) {
+        this.initials = initials;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Score{" +
+                "id=" + id +
+                ", initials='" + initials + '\'' +
+                ", points=" + points +
+                '}';
+    }
+}
